69ce881cd155e7c890c85014c6da6ea678f45ce4
Laksh <lakshtaneja02@gmail.com>
2025-07-17 18:56:48 +0530

    refactor settings to a function, add cli args and time flag

--- main.go
+++ main.go
@@ -2,21 +2,57 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
+type Settings map[string]map[string][]string
+
 func main() {
+	isTime := flag.Bool("time", false, "organize by date modified")
+	flag.Parse()
 
-	s := make(map[string]map[string][]string)
+	if len(flag.Args()) != 1 {
+		log.Fatal("Incorrect usage\nCorrect usage:\norganize <dir>\norganize --time <dir>")
+	}
 
+	dirInfo, err := os.Stat(flag.Arg(0))
+	if err != nil || !dirInfo.IsDir() {
+		log.Fatal("System couldn't find directory")
+	}
+
+	var s Settings
+	readSettings(&s)
+
+	files, err := os.ReadDir(dirInfo.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(files) == 0 {
+		fmt.Println("Done")
+		return
+	}
+
+	if *isTime {
+		return
+	} else {
+		organizeByType()
+	}
+
+}
+
+func readSettings(s *Settings) {
 	settings, err := os.ReadFile("./settings.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := json.Unmarshal(settings, &s); err != nil {
+	if err := json.Unmarshal(settings, s); err != nil {
 		log.Fatal(err)
 	}
+}
+func organizeByType() {
 
 }
