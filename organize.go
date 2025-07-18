d18310b21e8ccf1d00e96765a5e1407686486bfe
Laksh <lakshtaneja02@gmail.com>
2025-07-18 16:19:57 +0530

    update organizeByType, maintain a folderLogs array for logging files renames files to include subfolder directory

--- organize.go
+++ organize.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 )
 
 func organizeByType(rootDir os.FileInfo, files []os.DirEntry, s Settings) {
+	folderLogs := make([]folderLog, 0, 100)
+
 	for _, entry := range files {
 		if entry.IsDir() {
 			continue
 		}
-		folder := checkFolder(s, filepath.Ext(entry.Name()))
-		fmt.Println(folder, entry.Name())
+		folderName := checkFolderName(s, filepath.Ext(entry.Name()))
+		dir := rootDir.Name() + folderName
+
+		oldPath := rootDir.Name() + "/" + entry.Name()
+		newPath := dir + "/" + entry.Name()
+
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			log.Fatal(err)
+
+		}
+		if err := os.Rename(oldPath, newPath); err != nil {
+			log.Fatal(err)
+		}
+		if exist, pos := checkFolderLogs(folderLogs, folderName); exist {
+			folderLogs[pos].count++
+		} else {
+			folderLogs = append(folderLogs, folderLog{folderName, 1})
+		}
 	}
+	printFolderLogs(folderLogs)
 }
