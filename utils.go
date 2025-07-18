c6c092cd539c673363d6bb0f06eed371f6986967
Laksh <lakshtaneja02@gmail.com>
2025-07-18 16:16:52 +0530

    add folderLog struct for logging the no of files pushed to a new subfolder, add checkFolderLogs() for checking if a folder has been pushed to the active logs array and, printFolderLogs() for printing a formatted version of logs to stdout

--- utils.go
+++ utils.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
-func checkFolder(s Settings, ext string) string {
+type folderLog struct {
+	name  string
+	count int
+}
+
+func checkFolderName(s Settings, ext string) string {
 	for f := range s["type"] {
 		for i := 0; i < len(s["type"][f]); i++ {
 			if ext == s["type"][f][i] {
@@ -25,5 +31,20 @@ func readSettings(s *Settings) {
 
 	if err := json.Unmarshal(settings, s); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func checkFolderLogs(folderLogs []folderLog, folder string) (bool, int) {
+	for i, folderLog := range folderLogs {
+		if folderLog.name == folder {
+			return true, i
+		}
+	}
+	return false, 0
+}
+
+func printFolderLogs(folderLog []folderLog) {
+	for _, folderCount := range folderLog {
+		fmt.Println("\t", folderCount.count, "files", "=>", folderCount.name)
 	}
 }
