c165fa51d338db3bc6e43cb736e43745c7e0870f
Laksh <lakshtaneja02@gmail.com>
2025-07-26 18:13:46 +0530

    update organizeByDate, divide by year, append month if option, log by foldeLogs and printFolderLogs

--- organize.go
+++ organize.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func organizeByType(rootDir os.FileInfo, files []os.DirEntry, s map[string][]string) {
@@ -36,5 +37,43 @@ func organizeByType(rootDir os.FileInfo, files []os.DirEntry, s map[string][]str
 }
 
 func organizeByDate(rootDir os.FileInfo, files []os.DirEntry, s Time) {
+	folderLogs := make([]folderLog, 0, 100)
+	for _, entry := range files {
+		if entry.IsDir() {
+			continue
+		}
+		file, err := entry.Info()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 
+		var folderName string
+
+		if s.Monthly {
+			folderName = file.ModTime().Month().String() + " "
+		}
+
+		folderName += strconv.Itoa(file.ModTime().Year())
+
+		dir := rootDir.Name() + "/" + folderName
+
+		oldPath := rootDir.Name() + "/" + entry.Name()
+		newPath := dir + "/" + entry.Name()
+
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			log.Fatal(err)
+
+		}
+		if err := os.Rename(oldPath, newPath); err != nil {
+			log.Fatal(err)
+		}
+		if exist, pos := checkFolderLogs(folderLogs, folderName); exist {
+			folderLogs[pos].count++
+		} else {
+			folderLogs = append(folderLogs, folderLog{folderName, 1})
+		}
+	}
+
+	printFolderLogs(folderLogs)
 }
